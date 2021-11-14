d5f518735092122945ef4c2cff1f18077ceeffab
Gabriel Jones <gjones2023@chadwickschool.org>
2021-11-13 17:34:58 -0800

    docs: Add comments and formatting
    This change is being made to finish the project

--- DigitsOfPi/src/PiCounter.java
+++ DigitsOfPi/src/PiCounter.java
@@ -1,29 +1,52 @@
 import java.io.IOException;
 
+/**
+ * Represents frequencies of numbers 0-9 in a text file and notes the number of
+ * non-digit characters
+ * 
+ * @author gabrieljones
+ *
+ */
 public class PiCounter extends TextFileAccessor {
-	private final int ASCII_CODE_0 = 48;
-	private int mistakes;
-	private int[] counts = new int[10];
+	private final int ASCII_CODE_0 = 48; // ASCII code value for the number 0
+	private int mistakes; // holds integer number of current mistakes in the text
+	private int[] counts; // holds count of each number from 0-9
+
+	/**
+	 * Constructs a PiDCounter object based on file if the file name exists,
+	 * otherwise throws IOException
+	 * 
+	 * @param filename name of the file
+	 * @throws IOException if input file name does not exist
+	 */
 	public PiCounter(String filename) throws IOException {
 		openFile(filename);
-		// TODO: initialize variables in constructor
 		mistakes = 0;
+		counts = new int[10];
 	}
+
 	public void processLine(String curLine) {
+		// creates character array of the input string
 		char[] currentLine = curLine.toCharArray();
-		for(int i = 0; i < currentLine.length; i++) {
-			if(!Character.isDigit(currentLine[i])) {
+		/* for every character in the line, checks if the character is a number, if it
+		   is it adds to the respective index of the counts array, otherwise it adds to
+		   the mistakes*/
+		for (int i = 0; i < currentLine.length; i++) {
+			if (!Character.isDigit(currentLine[i])) {
 				mistakes++;
-			}
-			else {
-				counts[currentLine[i]-ASCII_CODE_0]++;
+			} else {
+				counts[currentLine[i] - ASCII_CODE_0]++;
 			}
 		}
 	}
+
 	public void printReport() {
-		for(int i = 0; i < counts.length; i++) {
+		/* for every number in the counts array, prints the respective number, and count
+		   of that number in the inputed text file */
+		for (int i = 0; i < counts.length; i++) {
 			System.out.println(i + " " + counts[i]);
 		}
-		System.out.println("bad symbols: "+ mistakes);
+		// prints number of mistakes
+		System.out.println("bad symbols: " + mistakes);
 	}
 }
--- DigitsOfPi/src/PiDriver.java
+++ DigitsOfPi/src/PiDriver.java
@@ -1,7 +1,16 @@
+/**
+ * If given a text file, run Pi Counter and process the file to find the
+ * frequencies of the numbers 0-9 and to find the number of non digit
+ * characters. Then print each number's frequency and the number of non digit
+ * characters.
+ * 
+ * @author gabrieljones
+ *
+ */
 public class PiDriver {
 	public static void main(String args[]) {
 		try {
-			PiCounter e = new PiCounter("corruptedDigitsOfPi.txt");
+			PiCounter e = new PiCounter("e.txt");
 			e.processFile();
 			e.printReport();
 		} catch (Exception e) {
